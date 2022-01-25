8f4a73be0b72fefc25911156a87317336ac9cf11
navid.shokri <shokri@sabacell.ir>
2022-01-25 15:01:35 +0330

    refactor code remove useless included name space

--- NewsLetterBoy.Repository/NewsLetterDbContext.cs
+++ NewsLetterBoy.Repository/NewsLetterDbContext.cs
@@ -1,6 +1,5 @@
 using System.Collections.Generic;
 using Microsoft.EntityFrameworkCore;
-using NewsLetterBoy.Model;
 using NewsLetterBoy.Model.NewsLetter;
 using NewsLetterBoy.Model.Subscription;
 
--- NewsLetterBoy.Repository/NewsLetterRepository.cs
+++ NewsLetterBoy.Repository/NewsLetterRepository.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Logging;
-using NewsLetterBoy.Model;
+﻿using Microsoft.Extensions.Logging;
 using NewsLetterBoy.Model.NewsLetter;
 
 namespace NewsLetterBoy.Repository
